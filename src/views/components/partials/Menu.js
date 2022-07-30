792d6c2ac7ef2a5b3658881048558eb6a86e7fbb
Tauseef Ahamed <tauseefahamed14@gmail.com>
2022-07-30 13:15:39 +0530

    Permissionn restriction updated

--- src/views/components/partials/Menu.js
+++ src/views/components/partials/Menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { checkPermission } from "../../../app/hooks/with-permission";
 import { MenuItem } from "./MenuItem";
 
 export class Menu extends React.Component {
@@ -8,18 +9,28 @@ export class Menu extends React.Component {
         <MenuItem className="icon-dashboard" to="/" active="true">
           Dashboard
         </MenuItem>
-        <MenuItem className="icon-barns" to="/barns">
-          Barns
-        </MenuItem>
-        <MenuItem className="icon-devices" to="/devices">
-          Devices
-        </MenuItem>
-        <MenuItem className="icon-alerts" to="/alerts">
-          Alerts
-        </MenuItem>
-        <MenuItem className="icon-settings" to="/settings">
-          Settings
-        </MenuItem>
+        {checkPermission("view-barn") && (
+          <MenuItem className="icon-barns" to="/barns">
+            Barns
+          </MenuItem>
+        )}
+        {checkPermission("view-devices") && (
+          <MenuItem className="icon-devices" to="/devices">
+            Devices
+          </MenuItem>
+        )}
+        {checkPermission("receive-notifications") && (
+          <MenuItem className="icon-alerts" to="/alerts">
+            Alerts
+          </MenuItem>
+        )}
+        {(checkPermission("change-password") ||
+          checkPermission("send-feedback")) && (
+          <MenuItem className="icon-settings" to="/settings">
+            Settings
+          </MenuItem>
+        )}
+
         {window.innerWidth < 768 && (
           <MenuItem className="icon-logout" to="/logout">
             Logout
